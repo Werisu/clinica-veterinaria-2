e28370cd6c2520953d10ea88f0a0b08b1a05a844
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-04-09 20:54:35 -0300

    feat(layout): integrates header component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { LayoutModule } from '@clinica-veterinaria-2/layout';
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
+      imports: [AppComponent, RouterTestingModule, LayoutModule],
     }).compileComponents();
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome clinica-veterinaria-2'
-    );
   });
 
   it(`should have as title 'clinica-veterinaria-2'`, () => {
