198a4516a5b7ab7a6cc0e163116d83e8a836cf72
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-04-27 18:43:38 -0300

    feat: add angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), importProvidersFrom(HttpClientModule)],
+  providers: [
+    provideRouter(appRoutes),
+    importProvidersFrom(HttpClientModule),
+    provideAnimationsAsync(),
+  ],
 };
