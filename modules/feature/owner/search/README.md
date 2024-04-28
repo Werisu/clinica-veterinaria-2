941bffc1790c238caee52a545f9221122cf10391
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-04-28 12:59:40 -0300

    feat(owner-search): add lib

---
+++ modules/feature/owner/search/README.md
@@ -0,0 +1,7 @@
+# owner-search
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test owner-search` to execute the unit tests.
---
+++ modules/feature/owner/search/src/lib/owner-search/owner-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-owner-search',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './owner-search.component.html',
+  styleUrl: './owner-search.component.scss',
+})
+export class OwnerSearchComponent {}
